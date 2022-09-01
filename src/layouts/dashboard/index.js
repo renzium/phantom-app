8758b4fee7dcb452ba559f43b28630f4b1e76f60
RajaSabiq <ther.sabiqarfan@gmail.com>
2022-09-02 03:12:56 +0500

    Update

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -144,18 +144,7 @@ function Dashboard() {
             </Grid>
           </Grid>
         </MDBox>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              <Projects />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <OrdersOverview />
-            </Grid>
-          </Grid>
-        </MDBox>
       </MDBox>
-      <Footer />
     </DashboardLayout>
   );
 }
